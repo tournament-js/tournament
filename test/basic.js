e6d7977a57d3959e95e66c0bd3c57118f7fffebd
clux <analsandblaster@gmail.com>
2012-08-08 22:39:22 +0100

    add robin tests + beef up the group tests significantly

--- test/basic.js
+++ test/basic.js
@@ -3,10 +3,10 @@ var tap = require('tap')
   , $ = require('interlude')
   , T = require('../');
 
-test("group properties", function (t) {
+test("groups", function (t) {
   // TODO: eventually buff these numbers to test all cases..
-  var nums = $.range(10)
-    , gsizes = $.range(4);
+  var nums = $.range(200)
+    , gsizes = $.range(16);
 
   nums.forEach(function (np) {
     gsizes.forEach(function (s) {
@@ -24,12 +24,46 @@ test("group properties", function (t) {
 
         if ($.even(s)) {
           // a group is perfect if groups are full and only filled with pairs!
-          t.equal($.minimum(gsums), $.maximum(gsums), "sum of seeds zero in perfect groups");
+          t.equal($.minimum(gsums), $.maximum(gsums), "sum of seeds zero difference in perfect groups");
         }
       }
-
     });
   });
+  t.end();
+});
 
+test("robin", function (t) {
+  $.range(20).forEach(function (n) {
+    var rs = T.robin(n);
+    var expected = ($.odd(n)) ? n : n-1;
+    t.equal(expected, rs.length, "correct number of rounds");
+
+    var pMaps = []
+    $.range(n).forEach(function (p) {
+      pMaps[p] = []
+    });
+
+    rs.forEach(function (rnd) {
+      t.equal(rnd.length, Math.floor(n/2), "each round has correct number of matches");
+
+      var plrs = $.flatten(rnd);
+      t.deepEqual(plrs, $.nub(plrs), "players listed only once per round");
+
+      // keep track of who everyone is playing as well
+      rnd.forEach(function (p) {
+        var a = p[0]
+          , b = p[1];
+        pMaps[a].push(b);
+        pMaps[b].push(a);
+      });
+    });
+
+    Object.keys(pMaps).forEach(function (p) {
+      var val = pMaps[p].sort($.compare());
+      var exp = $.delete($.range(n), Number(p));
+      // if this true, then each play all exactly once by previous test
+      t.deepEqual(val, exp, "player " + p + " plays every enemy");
+    });
+  });
   t.end();
 });
