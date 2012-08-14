67ab5b51abdca437792582a855a8a86ae93b471e
clux <analsandblaster@gmail.com>
2012-08-14 20:24:02 +0100

    duel scoring, duel placement helper and ffaElimination constructor with new next-round calculation algorithm

--- test/elimination.js
+++ test/elimination.js
@@ -3,17 +3,62 @@ var tap = require('tap')
   , $ = require('interlude')
   , T = require('../');
 
-test("elimination", function (t) {
-  // try scoring everything in order
-  var gs = T.elimination(5, T.LB);
-  gs.forEach(function (g) {
-    T.scoreDuel(gs, 3, T.LB, g.id, [0, 1]);
-  });
+test("placement", function (t) {
+  t.equal(T.placement(T.WB, 2, 1), 3, "placement 2 1");
+  t.equal(T.placement(T.WB, 2, 2), 2, "placement 2 2");
+  t.equal(T.placement(T.WB, 2, 3), 1, "placement 2 3");
 
-  var lastPls = gs[gs.length-1].p.filter(function (n) {
-    return n !== 0;
-  });
-  t.equal(lastPls.length, 2, "got two players at the end when scoring everything");
+  t.equal(T.placement(T.WB, 3, 1), 5, "placement 3 1");
+  t.equal(T.placement(T.WB, 3, 2), 3, "placement 3 2");
+  t.equal(T.placement(T.WB, 3, 3), 2, "placement 3 3");
+  t.equal(T.placement(T.WB, 3, 4), 1, "placement 3 4");
+
+  t.equal(T.placement(T.WB, 4, 5), 1, "placement 4 5");
+  t.equal(T.placement(T.WB, 4, 4), 2, "placement 4 4");
+
+  t.equal(T.placement(T.LB, 2, 1), 4, "placement LB 2 1");
+  t.equal(T.placement(T.LB, 2, 2), 3, "placement LB 2 2");
+  t.equal(T.placement(T.LB, 2, 3), 2, "placement LB 2 3");
+  t.equal(T.placement(T.LB, 2, 4), 2, "placement LB 2 4");
+  t.equal(T.placement(T.LB, 2, 5), 1, "placement LB 2 5");
+
+  t.equal(T.placement(T.LB, 3, 1), 7, "placement LB 3 1");
+  t.equal(T.placement(T.LB, 3, 2), 5, "placement LB 3 2");
+  t.equal(T.placement(T.LB, 3, 3), 4, "placement LB 3 3");
+  t.equal(T.placement(T.LB, 3, 4), 3, "placement LB 3 4");
+  t.equal(T.placement(T.LB, 3, 5), 2, "placement LB 3 5");
+  t.equal(T.placement(T.LB, 3, 6), 2, "placement LB 3 6");
+  t.equal(T.placement(T.LB, 3, 7), 1, "placement LB 3 7");
+
+  t.equal(T.placement(T.LB, 4, 8), 2, "placement LB 4 8");
+  t.equal(T.placement(T.LB, 4, 9), 1, "placement LB 4 9");
 
   t.end();
 });
+
+test("elimination", function (t) {
+  // try scoring everything in order
+  var gs = T.elimination(T.LB, 5);
+  var p = 3;
+  var lastM = gs[gs.length-1];
+  t.ok(!lastM.m, "no map scores recorded for last match yet");
+
+  gs.forEach(function (g) {
+    // will produce some warnings when there are WO markers present
+    T.scoreDuel(T.LB, p, gs, g.id, [0, 2]);
+  });
+
+  t.ok(lastM.m, "map scores recorded for last match");
+  var lastPls = lastM.p.filter(function (n) {
+    return (n !== 0 && n !== T.WO);
+  });
+  // note this only true because this case gets a double final
+  t.equal(lastPls.length, 2, "got two players at the end when scoring everything");
+
+  var res = T.duelResults(T.LB, 3, gs);
+  //t.deepEqual(res, {}, "r"); // TODO: proper test that things progressed as expected
+  t.ok(res, "results produced");
+
+  t.end();
+});
+
--- tournament.js
+++ tournament.js
@@ -3,24 +3,6 @@ var $ = require('interlude') // should only need autonomy + compare
   , eql = require('deep-equal')
   , T = {}; // exports
 
-// helper fn
-var even_seed = function (i, n) {
-  var k = Math.floor(Math.log(i) / Math.log(2))
-    , r = i - Math.pow(2, k);
-  if (r === 0) {
-    return Math.pow(2, n - k);
-  }
-  var nr = (i - 2*r).toString(2).split('').reverse().join('');
-  return (parseInt(nr, 2) << n - nr.length) + Math.pow(2, n - k - 1);
-};
-
-// returns the seeds in match i when the match is played in a tournament of size 2**pow
-// NB: match_nr is 1-indexed [passing i=0 -> massive negative number]
-var seeds = function (i, pow, num_players) {
-  var even = even_seed(i, pow);
-  return [Math.pow(2, pow) + 1 - even, even];
-};
-
 T.groups = function (num_players, size) {
   if (!num_players || !size) {
     return [];
@@ -60,7 +42,7 @@ T.groups = function (num_players, size) {
   });
 };
 
-// returns an array of round representation arrays. Each containing all matches to be played in one round, filling in seeding numbers as player placeholders
+// returns an array of round representation arrays. Each containing all games to be played in one round, filling in seeding numbers as player placeholders
 // each match array (the innermost one) if of the form [player1, player2]
 // follows http://en.wikipedia.org/wiki/Round-robin_tournament#Scheduling_algorithm
 T.robin = function (n) {  // num players
@@ -71,26 +53,27 @@ T.robin = function (n) {  // num players
     ps.push(-1); // add dummy player to match algorithm for even numbers
     n += 1;
   }
-
   for (var j = 0; j < n - 1; j += 1) {
     rs[j] = []; // create inner match array for round j
     for (var i = 0; i < n / 2; i += 1) {
-      if (ps[i] >= 0 && ps[n - 1 - i] >= 0) {
-        rs[j].push([ps[i], ps[n - 1 - i]]); // insert a full pair
+      if (ps[i] !== -1 && ps[n - 1 - i] !== -1) { // both players are non-dummies
+        rs[j].push([ps[i], ps[n - 1 - i]]); // insert the pair as a game
       }
     }
     ps.splice(1, 0, ps.pop()); // permutate for next round
   }
-
   return rs;
 };
 
-// -----------------------------------
-// constants for tournaments
-const WB = 1
-    , LB = 2;
+// -----------------------------------------------------------------------------
+// general tournament helpers
 
-const WO = -1;
+// constants for tournaments
+/*const WB = 1
+    , LB = 2
+    , WO = -1;*/
+
+var WB = 1, LB = 2, WO = -1;
 
 // export constants
 T.WB = WB;
@@ -116,14 +99,35 @@ T.byId = function (id, g) {
 
 // sorting is chronological, and as you normally read it: WB R2 G3
 // => first bracket, then round, then game.
-var compareMatches = function (g1, g2) {
+var compareGames = function (g1, g2) {
   return (g1.id.b - g2.id.b) || (g1.id.r - g2.id.r) || (g1.id.g - g2.id.g);
 };
 
-// return an array of matches for a tournament
+// -----------------------------------------------------------------------------
+// duel elimination stuff
+
+// helpers to initialize duel tournaments
+var even_seed = function (i, n) {
+  var k = Math.floor(Math.log(i) / Math.log(2))
+    , r = i - Math.pow(2, k);
+  if (r === 0) {
+    return Math.pow(2, n - k);
+  }
+  var nr = (i - 2*r).toString(2).split('').reverse().join('');
+  return (parseInt(nr, 2) << n - nr.length) + Math.pow(2, n - k - 1);
+};
+
+// returns the seeds in match i when the match is played in a tournament of size 2**pow
+// NB: match_nr is 1-indexed [passing i=0 -> massive negative number]
+var seeds = function (i, pow, num_players) {
+  var even = even_seed(i, pow);
+  return [Math.pow(2, pow) + 1 - even, even];
+};
+
+// return an array of games for a tournament
 // a match has the form {p: playerArray, b: bracketNum, r: roundNum, g: gameNum}
 // bracket, round and game number are 1 indexed
-T.elimination = function (num_players, last) {
+T.elimination = function (last, num_players) {
   if (num_players < 4) {
     return [];
   }
@@ -133,18 +137,17 @@ T.elimination = function (num_players, last) {
 
   var n = Math.ceil(Math.log(num_players) / Math.log(2))
     , num = Math.pow(2, n)    // true model number
-    , matches = [];           // match array
+    , games = [];           // match array
 
   // create round 1,2 in WB & LB
   (function init() {
-    // placeholders for progression matches in WBR2, LBR1 & LBR2 used in even iterations,
+    // placeholders for progression games in WBR2, LBR1 & LBR2 used in even iterations,
     var lbm1, lbm2, wbm2;
 
     for (var i = 1; i <= num / 2; i += 1) {
       var ps = woMark(seeds(i, n), num_players)
-        , isEven = Number(i % 2 === 0);
-
-      matches.push({id: gId(WB, 1, i), p: ps}); // wbm1
+        , isEven = Number(i % 2 === 0)
+        , wbm1 = {id: gId(WB, 1, i), p: ps}; // this gets one per iteration
 
       // create shells for WBR2, LBR1 & LBR2
       if (!isEven) {
@@ -159,20 +162,23 @@ T.elimination = function (num_players, last) {
 
       if (ps[0] === WO || ps[1] === WO) {
         wbm2.p[isEven] = ps[Number(ps[0] === WO)]; // advance winner
+        wbm1.m = (ps[0] === WO) ? [0, 1] : [1, 0]; // set WO score in wbm1
 
         if (last >= LB) {
           lbm1.p[isEven] = WO; // wo marker 'lost', so goes to LBR1
           if (lbm1.p[0] === WO && lbm1.p[1] === WO) { // only true in the second iteration => i is even
             lbm2.p[Number(!isEven)] = WO; // rare, double wo marker in LBR1 (uses reverse positioning to LBR1)
+            lbm1.m = [1, 0]; // randomly score one as the winner
           }
         }
       }
 
-      // shells pushed to matches every other iteration
+      games.push(wbm1);
+      // progressed shells pushed to games every other iteration
       if (isEven) {
-        matches.push(wbm2);
+        games.push(wbm2);
         if (last >= LB) {
-          matches.push(lbm1, lbm2);
+          games.push(lbm1, lbm2);
         }
       }
     }
@@ -182,24 +188,24 @@ T.elimination = function (num_players, last) {
   if (n >= 3) {
     for (var r = 3; r <= n; r += 1) {
       for (var g = 1; g <= Math.pow(2, n - r); g += 1) {
-        matches.push({id: gId(WB, r, g), p: [0, 0]});
+        games.push({id: gId(WB, r, g), p: [0, 0]});
       }
     }
   }
 
   if (last >= LB) {
-    if (n > 2) { // otherwise we only need the final matches added
+    if (n > 2) { // otherwise we only need the final games added
       for (var r = 3; r <= 2*n - 2; r += 1) {
-        // number of matches halves every odd round in losers bracket
+        // number of games halves every odd round in losers bracket
         for (var g = 1; g <= Math.pow(2, n - 1 - Math.floor((r + 1) / 2)); g += 1) {
-          matches.push({id: gId(LB, r, g), p: [0, 0]});
+          games.push({id: gId(LB, r, g), p: [0, 0]});
         }
       }
     }
-    matches.push({id: gId(LB, 2*n - 1, 1), p: [0, 0]}); // grand final game 1
-    matches.push({id: gId(LB, 2*n, 1), p: [0, 0]});     // grand final game 2
+    games.push({id: gId(LB, 2*n - 1, 1), p: [0, 0]}); // grand final game 1
+    games.push({id: gId(LB, 2*n, 1), p: [0, 0]});     // grand final game 2
   }
-  return matches.sort(compareMatches);
+  return games.sort(compareGames); // sort so they can be scored in order
 };
 
 T.representation = function (id) {
@@ -215,7 +221,7 @@ T.representation = function (id) {
 };
 
 // progression helpers, winner in `id` goes right to returned id or tournament over
-T.right = function (p, last, id, longLbGf) {
+T.right = function (last, p, id, longLbGf) {
   var b = id.b, r = id.r, g = id.g;
 
   if (r < 1 || g < 1) {
@@ -260,38 +266,34 @@ T.right = function (p, last, id, longLbGf) {
   return [gId(b, r + 1, ghalf), pos];
 };
 
-T.down = function (p, last, id, longLbGf) {
+T.down = function (last, p, id, longLbGf) {
   var b = id.b, r = id.r, g = id.g;
 
   // is player out?
   if (last === WB) {
     return null;
   }
-
   if (r === 2*p - 1) {
     // if double final, then loser moves to the bottom
     return (longLbGf) ? [gId(LB, 2 * p, 1), 1] : null;
   }
-
-  // remaining out cases + invalids
   if (b === LB || r > p || r < 1) {
+    // remaining out cases + invalids
     return null;
   }
 
-  var ghalf = Math.floor((g + 1) / 2);
   // drop on top >R2 and <=2 for odd g to match bracket movement
   var pos = (r > 2 || $.odd(g)) ? 0 : 1;
 
   if (r === 1) {
+    var ghalf = Math.floor((g + 1) / 2);
     return [gId(LB, 1, ghalf), pos]; // LBR1 only gets input from WBR1
   }
   return  [gId(LB, (r - 1)*2, g), pos]; // WBRr -> 2x as late per round in WB
-
-
 };
 
-// updates matches by updating the given match, and propagate the winners/losers
-T.scoreDuel = function (gs, p, last, id, score) {
+// updates games by updating the given match, and propagate the winners/losers
+T.scoreDuel = function (last, p, gs, id, score) {
   // sanity
   if (!Array.isArray(score) || score.length !== 2)  {
     throw new Error("invalid scores: must be array of length 2 - got: " + JSON.stringify(score));
@@ -309,7 +311,7 @@ T.scoreDuel = function (gs, p, last, id, score) {
     console.error("cannot override score walkover'd game: " + T.representation(id));
     return gs;
   }
-  m.s = score;
+  m.m = score; // only map scores are relevant for progression
 
   // calculate winner and loser for progression
   var w = (score[0] > score[1]) ? m.p[0] : m.p[1]
@@ -318,7 +320,7 @@ T.scoreDuel = function (gs, p, last, id, score) {
   // did underdog win GF1? Then override and force progression fns to move to GF2.
   var longLbGf = (id.b === LB && id.r === 2*p - 1 && score[1] > score[0]);
 
-  //TODO: if right/down is truthy but nxtM/dnM is not, tournament is corrupt, warn!
+  // helper to insert player adv into [id, pos] and warn if the match does not exist
   var playerInsert = function (where, adv) {
     if (!where) {
       return; // nothing to do
@@ -335,7 +337,7 @@ T.scoreDuel = function (gs, p, last, id, score) {
     }
     insertM.p[pos] = adv;
     if (insertM.p[(pos + 1) % 2] === WO) {
-      // insertM.s = (pos) ? [0, 1] : [1, 0]; // set WO score (unnecessary)
+      insertM.m = (pos) ? [0, 1] : [1, 0]; // set WO map scores
 
       // return id and wo winner so that it can be forwarded on
       return [insertM.id, adv];
@@ -343,21 +345,171 @@ T.scoreDuel = function (gs, p, last, id, score) {
   };
 
   // 2. move winner right
-  var rprog = T.right(p, last, id, longLbGf);
-  playerInsert(rprog, w);
+  playerInsert(T.right(last, p, id, longLbGf), w);
 
   // 3. move loser down if applicable
-  var dprog = T.down(p, last, id, longLbGf);
-  var dres = playerInsert(dprog, l);
+  var dres = playerInsert(T.down(last, p, id, longLbGf), l);
 
   // 4. check if loser must be forwarded from existing WO in LBR1/LBR2
   // only WO fw case, non-WO match winner can never T.right onto a WO match
   if (dres) {
-    var dprog2 = T.right(p, last, dres[0], false);
-    playerInsert(dprog2, dres[1]);
+    playerInsert(T.right(last, p, dres[0], false), dres[1]);
   }
 
   return gs;
 };
+
+T.scorable = function (last, p, id) {
+  // check if the match with that id has both players and none of them are WOs
+  // check if T.next(p, last, id, true)
+  return false;
+};
+
+// players (potential) max round in the last bracket of a tournament -> placement
+T.placement = function (last, p, maxr) {
+  if (last === LB) {
+    if (maxr === 2*p + 1) return 1;
+    if (maxr === 2*p)     return 2;
+
+    var metric = 2*p + 1 - maxr;
+    var r = metric - 4;
+    var k = Math.floor((r + 1)/2);
+    var oddExtra = ($.odd(r)) ? 0 : Math.pow(2, k);
+    return (metric <= 4) ? metric : Math.pow(2, k + 1) + 1 + oddExtra;
+  }
+  // last === WB
+  return (maxr === p + 1) ? 1 : Math.pow(2, p - maxr) + 1;
+};
+
+// can be called at any time, but before the final game, it's just statistics
+// should be called at the end of each round at most though for sanity of stats..
+T.duelResults = function (last, p, gs) {
+  // size = maximal player number found in tournament (<= 2^p because of model fitting)
+  // all p exist in round one, so suffices to scan one of the first 2^(p-1) games
+  var size = $.maximum($.flatten($.pluck('p', gs.slice(0, Math.pow(2, p - 1)))));
+
+  var res = [];
+  for (var s = 0; s < size; s += 1) {
+    res[s] = {
+      seed : s + 1
+    , maps : 0
+    //, best : 0  // not clear what is best, need the system to come in here or reference instead
+    , wins : 0
+    , maxr : 1 // temporary stat to keep track of last round
+    };
+  }
+
+  for (var i = 0; i < gs.length; i += 1) {
+    var g = gs[i];
+    if (!g.m || g.p[0] === WO || g.p[1] === WO) {
+      continue; // only count played, non-wo'd games
+    }
+
+    // store zero indexed player ids to do the conversion once and for all
+    var p0 = g.p[0] - 1
+      , p1 = g.p[1] - 1
+      , w = (g.m[0] > g.m[1]) ? p0 : p1
+      , l = (g.m[0] > g.m[1]) ? p1 : p0;
+
+    // inc wins
+    res[w].wins += 1;
+    res[p0].maps += g.m[0];
+    res[p1].maps += g.m[1];
+
+    // max round a player got to
+    if (g.id.b === last) {
+      res[w].maxr = Math.max(res[w].maxr, g.id.r + 1);
+      res[l].maxr = Math.max(res[l].maxr, g.id.r);
+    }
+  }
+
+  // figure out position based on their max round..
+  for (var k = 0; k < res.length; k += 1) {
+    res[k].pos = T.placement(last, p, res[k].maxr);
+    delete res[k].maxr;
+  }
+
+  return res.sort($.comparing('pos'));
+};
+
+
+// enforce max power 7 for sensibility
+// => max r = 256 (in GFG2), max g = 128 (in R1), max p = 128
+/*var Game = new Schema({
+  id : {
+    b   : {type: Number, required: true, min: T.WB, max: T.LB}
+  , r   : {type: Number, required: true, min: 1, max: 256}
+  , g   : {type: Number, required: true, min: 1, max: 128}
+  }
+, r  : String // straingt from T.representation
+, d  : Date   // scheduled play date
+
+  // the following are ordered jointly and can be zipped
+, p  : [Number]   // should be between 1 and 128 to match nice seed numbers
+, m  : [Number]   // map wins, not actual map scores [if non-existent => not played]
+, s  : [[Number]] // scores list of map results one score list per player
+});
+
+// higher level structure
+var Tournament = new Schema({
+  games   : [Game]
+, ongoing : Boolean
+, results : [Results] // results array if !on
+, system  : Number    // scoring system: time asc, time desc, points asc, points desc
+, size    : Number    // number of players (keeps structure simple and disjoint from invitemaps..)
+});
+
+var Results = new Schema({
+  seed   : Number // in {1, .. , size}
+, pos    : Number // final tournament position
+, maps   : Number // number of maps taken
+, best   : Number // best game score (from all matching game.r entries)
+, wins   : Number // number of game wins
+});*/
+
+// -----------------------------------------------------------------------------
+// ffa tournaments
+
+T.ffaElimination = function (gs, adv, np) {
+  if (np <= 2 || gs <= 2 || np <= gs || adv >= gs || adv <= 0) {
+    console.error("invalid ffa configuration: players=" + np + ", groupsize=" + gs + ", advancing=" + adv);
+    return;
+  }
+  var matches = []
+    , grps = T.groups(np, gs);
+
+  // create each round iteratively
+  for (var r = 1; grps.length > 1; r += 1) {
+    for (var i = 1; i <= grps.length; i += 1) {
+      matches.push({id: {b: WB, r: r, g: i}, p: grps[i-1]});
+    }
+
+    // prepare for the next round, T.groups may adjust gs internally, we find best adv
+    var minsize = $.minimum($.pluck('length', grps));
+
+    // metric to determine the best adv for the next groups call uses 2 factors:
+    // - distance from requested adv => best - adv
+    // - how close the groups are to filled => (best * grps.length) % gs
+    // we weight these factors by 2 and 1 resp.
+    var bestA = Math.max(1, adv - (gs - minsize)); // ok starting point, 1 adv worst case fallback
+    var bestMetric = 2*Math.abs(bestA - adv) + ((bestA * grps.length) % gs)
+    for (var a = adv; a > 0; a -= 1) {
+      if (a >= minsize) {
+        continue; // need to actually eliminate something in each match..
+      }
+      var nMetric = 2*Math.abs(a - adv) + ((a * grps.length) % gs);
+      if (nMetric < bestMetric) { // better candidate
+        bestMetric = nMetric;
+        bestA = a;
+      }
+    }
+    grps = T.groups(grps.length * bestA, gs);
+  }
+  matches.push({id: {b: WB, r: r, g: 1}, p: grps[0]}); // grand final
+
+  return matches;
+};
+
+
 
 module.exports = T;
