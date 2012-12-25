6bd5ef23595102f3c01e2928c548f1847aff4f62
clux <analsandblaster@gmail.com>
2012-12-25 14:42:02 +0100

    refactor interface test a bit

--- test/trn.interface.test.js
+++ test/trn.interface.test.js
@@ -1,25 +1,30 @@
 var tap = require('tap')
   , test = tap.test
-  , T = require('../')
-  , KnockOut = require('../knockout')
-  , FFA = require('../ffa')
-  , GroupStage = require('../groupstage')
-  , Duel = require('../duel');
+  , T = require('../'); // main interface
+
+// individual class entry proints
+var mains = {
+  KnockOut : require('../knockout'),
+  FFA : require('../ffa'),
+  GroupStage : require('../groupstage'),
+  Duel : require('../duel')
+};
 
 var publics = ['WO', 'NA', 'WB', 'LB', 'TB', 'findMatch']
   , commonStatics = ['invalid', 'idString', 'fromJSON']
   , commonMethods = ['score', 'unscorable', 'upcoming', 'results', 'isDone']
   , commonMembers = ['matches', 'numPlayers'];
 
-var makeInstance = function (C, t) {
+var makeInstance = function (C) {
   switch (C.name) {
     case 'Duel':        return new C(4, 1);
     case 'FFA':         return new C(4, [4], []);
     case 'KnockOut':    return new C(5, [2]);
     case 'GroupStage':  return new C(8, 4);
-    default: t.ok(false, "invalid class: " + C.name);
+    default: throw new Error("invalid class: " + C.name);
   }
 };
+
 var classHasCommons = function (C, t) {
   commonStatics.forEach(function (s) {
     t.ok(C[s], s + " static exists on " + C.name);
@@ -31,52 +36,29 @@ var classHasCommons = function (C, t) {
     t.type(C.prototype[m], 'function', m + ' method is a function on ' + C.name);
   });
 
-  var inst = makeInstance(C, t);
+  var inst = makeInstance(C);
   commonMembers.forEach(function (m) {
     t.ok(inst[m] !== undefined, m + " member exists on inst of " + C.name);
     t.ok(inst[m] !== 'function', m + ' is a non-function on inst of ' + C.name);
   });
 };
 
-test("main interface", function (t) {
+var hasPublics = function (obj, t) {
   publics.forEach(function (p) {
-    t.ok(T[p] !== undefined, p + " prop on t exists");
+    t.ok(obj[p] !== undefined, p + " prop exists on " + obj.name);
   });
-  classHasCommons(T.KnockOut, t);
-  classHasCommons(T.FFA, t);
-  classHasCommons(T.Duel, t);
-  classHasCommons(T.GroupStage, t);
-  t.end();
-});
+};
 
-test("ko interface", function (t) {
-  publics.forEach(function (p) {
-    t.ok(KnockOut[p] !== undefined, p + " prop on KnockOut exists");
-  });
-  classHasCommons(KnockOut, t);
-  t.end();
-});
 
-test("ffa interface", function (t) {
-  publics.forEach(function (p) {
-    t.ok(FFA[p] !== undefined, p + " prop on FFA exists");
+test("interfaces", function (t) {
+  T.name = "main"; // for nice output
+  hasPublics(T, t); // main interface has publics outside class
+  ['Duel', 'FFA', 'GroupStage', 'KnockOut'].forEach(function (type) {
+    // both main and individual entry points should have commons
+    classHasCommons(T[type], t);
+    classHasCommons(mains[type], t);
+    // if we required individual entry point, then publics exist directly on that
+    hasPublics(mains[type], t);
   });
-  classHasCommons(FFA, t);
-  t.end();
-});
-
-test("duel interface", function (t) {
-  publics.forEach(function (p) {
-    t.ok(Duel[p] !== undefined, p + " prop on Duel exists");
-  });
-  classHasCommons(Duel, t);
-  t.end();
-});
-
-test("groupstage interface", function (t) {
-  publics.forEach(function (p) {
-    t.ok(GroupStage[p] !== undefined, p + " prop on GroupStage exists");
-  });
-  classHasCommons(GroupStage, t);
   t.end();
 });
