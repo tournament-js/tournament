99ff43a71c641737f4cba1a51b16be84722e5474
llafuente <llafuente@noboxout.com>
2013-09-22 16:00:18 +0200

    add isRoundDone, getRounds, getRound, getMatches, getMatch, getCurrentRound, getNextRound, getPosition, getPlayersIn

--- ffa.js
+++ ffa.js
@@ -1,7 +1,15 @@
 var FFA = require('./lib/ffa.js');
 var helpers = require('./lib/public');
+var T = require('./lib/common');
+
 Object.keys(helpers).forEach(function (key) {
-  FFA[key] = helpers[key];
+  if(key === "prototype") {
+    Object.keys(helpers.prototype).forEach(function (key) {
+      FFA.prototype[key] = helpers.prototype[key];
+    });
+  } else {
+    FFA[key] = helpers[key];
+  }
 });
 
 module.exports = FFA;
--- groupstage.js
+++ groupstage.js
@@ -1,7 +1,13 @@
 var GroupStage = require('./lib/groupstage.js');
 var helpers = require('./lib/public');
 Object.keys(helpers).forEach(function (key) {
-  GroupStage[key] = helpers[key];
+  if(key === "prototype") {
+    Object.keys(helpers.prototype).forEach(function (key) {
+      GroupStage.prototype[key] = helpers.prototype[key];
+    });
+  } else {
+    GroupStage[key] = helpers[key];
+  }
 });
 
 module.exports = GroupStage;
