f1d0002d4527dcc68bda8c549767bdc1a48eb8da
clux <analsandblaster@gmail.com>
2013-10-14 07:45:31 +0100

    add custom data testing to the parse test

--- test/trn.parse.test.js
+++ test/trn.parse.test.js
@@ -6,37 +6,42 @@ var test = require('tap').test
   , T = require('../');
 
 
-var create = function (type) {
+var create = function (Klass) {
+  var type = Klass.name;
   if (type === 'Duel') {
-    return new T.Duel(8, T.LB, {short: true});
+    return new Klass(8, T.LB, {short: true});
   }
   if (type === 'FFA') {
-    return new T.FFA(16, [4,4,4], [2,2]);
+    return new Klass(16, [4,4,4], [2,2]);
   }
   if (type === 'KnockOut') {
-    return new T.KnockOut(10, [2,4,2]);
+    return new Klass(10, [2,4,2]);
   }
   if (type === 'GroupStage') {
-    return new T.GroupStage(16, 4);
+    return new Klass(16, 4);
   }
   if (type === 'TieBreaker') {
     var gs = new T.GroupStage(16, 4);
     gs.matches.forEach(function (m) {
       gs.score(m.id, [1,0]);
     });
-    return new T.TieBreaker(gs.results(), 3);
+    return new Klass(gs.results(), 3);
   }
 };
 
 test("serialize/deserialize tournaments", function (t) {
   [T.Duel, T.FFA, T.KnockOut, T.GroupStage, T.TieBreaker].forEach(function (Klass) {
-    var inst = create(Klass.name);
+    var inst = create(Klass);
     t.ok(inst instanceof T.Base, "inst is a Base class instance");
     t.ok(inst instanceof Klass, "inst is a " + Klass.name + " instance");
+    inst.data = { hi: true };
+    inst.matches[0].data = { info: "arst" };
 
     var str = inst + '';
     var parsed = T.parse(str); // general parse
 
+    t.equal(parsed.data.hi, true, "saved a custom property on .data");
+    t.equal(parsed.matches[0].data.info, "arst", "saved a custom property on .data");
     t.ok(parsed instanceof T.Base, "parsed is a Base class instance");
     t.ok(parsed instanceof Klass, "parsed is a " + Klass.name + " instance");
 
@@ -46,12 +51,16 @@ test("serialize/deserialize tournaments", function (t) {
   // now parse directly on specific entry points
   // NB: no separate entry for tiebreaker yet as this makes no sense
   [EntryDuel, EntryFFA, EntryKnockOut, EntryGroupStage].forEach(function (Klass) {
-    var inst = create(Klass.name);
+    var inst = create(Klass);
     t.ok(inst instanceof Klass, "inst is a " + Klass.name + " instance");
+    inst.data = { hi: true };
+    inst.matches[0].data = { info: "arst" };
 
     var str = inst + '';
     var parsed = Klass.parse(str); // specific parse
 
+    t.equal(parsed.data.hi, true, "saved a custom property on .data");
+    t.equal(parsed.matches[0].data.info, "arst", "saved a custom property on .data");
     t.ok(parsed instanceof Klass, "parsed is a " + Klass.name + " instance");
 
     t.equal(str, parsed + '', "serialize the same way");
