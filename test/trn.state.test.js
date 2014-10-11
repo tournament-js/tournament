5ed609e3d78cd6d2ae416acc4444ce15b3fab942
clux <analsandblaster@gmail.com>
2014-10-11 21:44:52 +0100

    failing tests for new state interface

---
+++ test/trn.state.test.js
@@ -0,0 +1,15 @@
+var Challenge = require('./challenge');
+
+exports.serialize = function (t) {
+  var inst = new Challenge(2, {});
+  t.ok(inst instanceof Challenge, "inst is a " + Challenge.name + " instance");
+  t.ok(inst.matches.length, 'some matches must have been made');
+  inst.matches.forEach(function (m) {
+    inst.score(m.id, [1,0]);
+  });
+
+  var copy = Challenge.restore(2, {}, inst.state);
+  t.deepEqual(copy.matches, inst.matches, 'recreated matches');
+  t.deepEqual(copy.state, inst.state, 'state reassembled itself');
+  t.done();
+};
