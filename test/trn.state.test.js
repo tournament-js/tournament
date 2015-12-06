0ed2c4f65c0129824158bf38c86e5a5da73645bf
clux <analsandblaster@gmail.com>
2015-12-06 17:10:18 +0000

    more lint and try catch rewrites

--- test/trn.state.test.js
+++ test/trn.state.test.js
@@ -3,13 +3,13 @@ var Challenge = require('./challenge')
 
 test('serialize', function *(t) {
   var inst = new Challenge(2, {});
-  t.ok(inst instanceof Challenge, "inst is a " + Challenge.name + " instance");
+  t.instance(inst, Challenge, 'inst is a ' + Challenge.name + ' instance');
   t.ok(inst.matches.length, 'some matches must have been made');
   inst.matches.forEach(function (m) {
     inst.score(m.id, [1,0]);
   });
 
   var copy = Challenge.restore(2, {}, inst.state);
-  t.deepEqual(copy.matches, inst.matches, 'recreated matches');
-  t.deepEqual(copy.state, inst.state, 'state reassembled itself');
+  t.eq(copy.matches, inst.matches, 'recreated matches');
+  t.eq(copy.state, inst.state, 'state reassembled itself');
 });
--- test/trn.sub.test.js
+++ test/trn.sub.test.js
@@ -11,7 +11,7 @@ var SomeT = Base.sub('SomeT', function (opts, initParent) {
 });
 SomeT.prototype._verify = function (match) {
   if (match.id.m === 2) {
-    return "Cannot score match 2"; // for the lulz
+    return 'Cannot score match 2'; // for the lulz
   }
   return null;
 };
@@ -20,71 +20,63 @@ SomeT.prototype._progress = function (match) {
   this.opts.progressCalled(match);
 };
 SomeT.defaults = function (np, opts) {
-  opts = opts || {}; // NB: this is a hacky implementation not using .configure
-  opts.log = { error: function () {} }; // ignore logs
-  opts.progressCalled = opts.progressCalled || function () {};
-  return opts;
-};
-
-var attachInvalid = function () {
-  SomeT.invalid = function (numPlayers/*, opts*/) {
-    if (!Base.isInteger(numPlayers) || numPlayers < 2) {
-      return "Need at least 2 players";
-    }
-    return null;
-  };
+  // NB: this is a hacky implementation not using .configure - so preserve opts
+  var o = opts || {};
+  o.log = { error: function () {} }; // ignore logs
+  o.progressCalled = o.progressCalled || function () {};
+  return o;
 };
 
 test('sub', function *(t) {
-  t.plan(15); // so we know if some stuff never happened
-
   // ensure we have to implement invalid
-  try { new SomeT(4); }
-  catch (e) {
-    t.equal(e.message, 'SomeT must implement an Invalid function', 'missing');
+  var ctorCheck = function *() {
+    return new SomeT(1);
   }
+  yield t.throws(ctorCheck, /SomeT must implement an Invalid/, 'invalid fn missing');
 
-  // once we attach invalid, ensure it works
-  attachInvalid();
-  try { new SomeT(); }
-  catch (e) {
-    t.equal(e.message, 'Cannot construct SomeT: Need at least 2 players', 'inv');
-  }
+  // attach invalid fn manually
+  SomeT.invalid = function (numPlayers) {
+    if (!Base.isInteger(numPlayers) || numPlayers < 2) {
+      return 'Need at least 2 players';
+    }
+    return null;
+  };
+  yield t.throws(ctorCheck, /SomeT: Need at least 2 pl/, 'invalid rejected construct');
 
   // check verify score and unscorable
   var o = {
     progressCalled: function (match) {
-      t.ok(true, "progress was called");
-      t.deepEqual(match.m, [2,1], "match now has a score");
-      t.equal(match.id.m, 1, "it was the first match");
+      t.ok(true, 'progress was called');
+      t.eq(match.m, [2,1], 'match now has a score');
+      t.eq(match.id.m, 1, 'it was the first match');
     }
   };
   var inst = new SomeT(4, o);
 
   // score + unscorable
   t.ok(inst.matches.length, 'inst now set');
-  t.equal(inst.unscorable(inst.matches[0].id, [2,1]), null, "verify allows");
-  t.ok(inst.score(inst.matches[0].id, [2,1]), "could score");
+  t.eq(inst.unscorable(inst.matches[0].id, [2,1]), null, 'verify allows');
+  t.ok(inst.score(inst.matches[0].id, [2,1]), 'could score');
 
   var reason = inst.unscorable(inst.matches[1].id, [2,1]);
-  t.equal(reason, "Cannot score match 2", "verify rejects");
-  t.ok(!inst.score(inst.matches[1].id, [2,1]), "and thus score returns false");
+  t.eq(reason, 'Cannot score match 2', 'verify rejects');
+  t.ok(!inst.score(inst.matches[1].id, [2,1]), 'and thus score returns false');
 
 
   // helpers
   // isPlayable does not check verify obviously
-  t.ok(inst.isPlayable(inst.matches[0]), "can play first match");
-  t.ok(inst.isPlayable(inst.matches[1]), "can play second match");
-  t.equal(inst.findMatches({r:1}).length, 2, "both matches in r1");
-  t.deepEqual(inst.players(), [1,2,3,4], "4 players in tournament");
+  t.ok(inst.isPlayable(inst.matches[0]), 'can play first match');
+  t.ok(inst.isPlayable(inst.matches[1]), 'can play second match');
+  t.eq(inst.findMatches({r:1}).length, 2, 'both matches in r1');
+  t.eq(inst.players(), [1,2,3,4], '4 players in tournament');
 
   // results
   var res = inst.results();
-  t.deepEqual(res, [
+  t.eq(res, [
       { seed: 1, wins: 0, for: 0, against: 0, pos: 4 },
       { seed: 2, wins: 0, for: 0, against: 0, pos: 4 },
       { seed: 3, wins: 0, for: 0, against: 0, pos: 4 },
       { seed: 4, wins: 0, for: 0, against: 0, pos: 4 }
-    ], "default results"
+    ], 'default results'
   );
 });
